ffecdaad4d15cdc864932062848ea027543e2172
Zichao <33247399+666zcli@users.noreply.github.com>
2018-08-10 16:00:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship'
 
 # Model
 print('==> Building model..')
-# net = VGG('VGG19')
-net = ResNet18()
+net = VGG('VGG19')
+# net = ResNet18()
 # net = PreActResNet18()
 # net = GoogLeNet()
 # net = DenseNet121()
