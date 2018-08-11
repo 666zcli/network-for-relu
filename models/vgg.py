cd00cb56bb1517103f48525e4604b957a20f0406
Zichao <33247399+666zcli@users.noreply.github.com>
2018-08-11 12:26:03 -0400

    Update vgg.py

--- models/vgg.py
+++ models/vgg.py
@@ -33,7 +33,8 @@ class VGG(nn.Module):
             else:
                 layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
                            nn.BatchNorm2d(x),
-                           nn.ReLU(inplace=True)]
+                           #nn.ReLU(inplace=True)
+                           MLP(x)]
                 in_channels = x
         layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
         return nn.Sequential(*layers)
